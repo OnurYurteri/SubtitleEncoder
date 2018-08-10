eaaa0bbec7c5a8e3a68e3c206a9a61c29abfbe14
Onur Yurteri <yurterionur@gmail.com>
2018-08-10 19:36:36 +0300

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SubtitleEncoder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "Altyazı Seçiniz";
+            openFileDialog1.InitialDirectory = Path.GetDirectoryName(Application.ExecutablePath);
+            openFileDialog1.FileName = "";
+            openFileDialog1.Filter = "Altyazı Dosyaları (SRT,SUB,TXT)|*.SRT;*.SUB;*.TXT";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string dosyaYolu = openFileDialog1.FileName;
+                if (File.Exists(dosyaYolu) == true && (Path.GetExtension(dosyaYolu) == ".srt" || Path.GetExtension(dosyaYolu) == ".txt" || Path.GetExtension(dosyaYolu) == ".sub"))
+                {
+                    string text = File.ReadAllText(dosyaYolu, Encoding.Default);
+                    File.WriteAllText(dosyaYolu, text, Encoding.UTF8);
+                    MessageBox.Show("Altyazı Dönüştürüldü\n");
+                }
+                else
+                {
+                    MessageBox.Show("Sadece '.srt', '.sub' ve '.txt' formatları desteklenmektedir");
+                }
+            }
+            
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text;
+
+namespace SubtitleEncoder
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            if (args.Length > 0 && File.Exists(args[0]))
+            {
+                
+                string dosyaYolu = args[0];
+                if ((Path.GetExtension(dosyaYolu) == ".srt" || Path.GetExtension(dosyaYolu) == ".txt" || Path.GetExtension(dosyaYolu) == ".sub"))
+                {
+                    string text = File.ReadAllText(dosyaYolu, Encoding.Default);
+                    File.WriteAllText(dosyaYolu, text, Encoding.UTF8);
+                    MessageBox.Show("Altyazı Dönüştürüldü\nKrdş napıyon altyazıma geldi?");
+                }
+                else
+                {
+                    MessageBox.Show("Sadece '.srt', '.sub' ve '.txt' formatları desteklenmektedir");
+                }
+                
+            }
+            else
+            {
+                Application.Run(new Form1());
+            }
+        }
+    }
+}
